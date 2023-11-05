008888c8d9a3ef0795fb665e39c10018d748c473
Vetle de Vries <vetle1608@gmail.com>
2023-11-05 20:04:52 +0100

    Update sign-in-button.tsx

--- src/app/login/sign-in-button.tsx
+++ src/app/login/sign-in-button.tsx
@@ -8,11 +8,7 @@ import { Button } from '~/components/button'
 export function SignInButton() {
 	return (
 		<Button
-			onClick={() => {
-				signIn('discord', {
-					callbackUrl: '/'
-				})
-			}}
+			onClick={() => signIn('discord', { callbackUrl: '/' })}
 			className='flex w-fit items-center justify-center gap-2 bg-[#37393e] py-4 text-lg text-[#dcddde]'>
 			Log in with Discord
 			<FaDiscord className='h-6 w-6' />
