de7273851d075fc4e072756f6aed9831695254fd
Vetle de Vries <vetle1608@gmail.com>
2023-11-05 21:21:16 +0100

    revert trpc changes

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,37 +1,24 @@
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { type NextRequest } from "next/server";
 
-import { appRouter } from '~/server/api/root'
-import { createTRPCContext } from '~/server/api/trpc'
-import { auth } from '~/server/auth'
+import { env } from "~/env.mjs";
+import { appRouter } from "~/server/api/root";
+import { createTRPCContext } from "~/server/api/trpc";
 
-function setCorsHeaders(res: Response) {
-	res.headers.set('Access-Control-Allow-Origin', '*')
-	res.headers.set('Access-Control-Request-Method', '*')
-	res.headers.set('Access-Control-Allow-Methods', 'OPTIONS, GET, POST')
-	res.headers.set('Access-Control-Allow-Headers', '*')
-}
+const handler = (req: NextRequest) =>
+  fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    createContext: () => createTRPCContext({ req }),
+    onError:
+      env.NODE_ENV === "development"
+        ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+            );
+          }
+        : undefined,
+  });
 
-export function OPTIONS() {
-	const response = new Response(null, {
-		status: 204
-	})
-	setCorsHeaders(response)
-	return response
-}
-
-const handler = auth(async (req) => {
-	const response = await fetchRequestHandler({
-		endpoint: '/api/trpc',
-		router: appRouter,
-		req,
-		createContext: () => createTRPCContext({ auth: req.auth, req }),
-		onError({ error, path }) {
-			console.error(`>>> tRPC Error on '${path}'`, error)
-		}
-	})
-
-	setCorsHeaders(response)
-	return response
-})
-
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -1,72 +1,76 @@
 /**
  * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
- * 1. You want to modify request context (see Part 1)
- * 2. You want to create a new middleware or type of procedure (see Part 3)
+ * 1. You want to modify request context (see Part 1).
+ * 2. You want to create a new middleware or type of procedure (see Part 3).
  *
- * tl;dr - this is where all the tRPC server stuff is created and plugged in.
- * The pieces you will need to use are documented accordingly near the end
+ * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
+ * need to use are documented accordingly near the end.
  */
+
+import { type NextRequest } from 'next/server'
 import { initTRPC, TRPCError } from '@trpc/server'
-import { Session } from 'next-auth'
+import superjson from 'superjson'
 import { ZodError } from 'zod'
 
 import { auth } from '~/server/auth'
 import { db } from '~/server/db'
-import { transformer } from '~/trpc/shared'
 
 /**
  * 1. CONTEXT
  *
- * This section defines the "contexts" that are available in the backend API
+ * This section defines the "contexts" that are available in the backend API.
  *
- * These allow you to access things like the database, the session, etc, when
- * processing a request
- *
+ * These allow you to access things when processing a request, like the database, the session, etc.
  */
+
 interface CreateContextOptions {
-	session: Session | null
+	headers: Headers
 }
 
 /**
- * This helper generates the "internals" for a tRPC context. If you need to use
- * it, you can export it from here
+ * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
+ * it from here.
  *
  * Examples of things you may need it for:
- * - testing, so we dont have to mock Next.js' req/res
- * - trpc's `createSSGHelpers` where we don't have req/res
- * @see https://create.t3.gg/en/usage/trpc#-servertrpccontextts
+ * - testing, so we don't have to mock Next.js' req/res
+ * - tRPC's `createSSGHelpers`, where we don't have req/res
+ *
+ * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
  */
-const createInnerTRPCContext = (opts: CreateContextOptions) => {
+export const createInnerTRPCContext = async (opts: CreateContextOptions) => {
+	const session = await auth()
+
 	return {
-		session: opts.session,
+		session,
+		headers: opts.headers,
 		db
 	}
 }
 
 /**
- * This is the actual context you'll use in your router. It will be used to
- * process every request that goes through your tRPC endpoint
- * @link https://trpc.io/docs/context
+ * This is the actual context you will use in your router. It will be used to process every request
+ * that goes through your tRPC endpoint.
+ *
+ * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = async (opts: { req?: Request; auth: Session | null }) => {
-	const session = opts.auth ?? (await auth())
-	const source = opts.req?.headers.get('x-trpc-source') ?? 'unknown'
+export const createTRPCContext = async (opts: { req: NextRequest }) => {
+	// Fetch stuff that depends on the request
 
-	console.log('>>> tRPC Request from', source, 'by', session?.user)
-
-	return createInnerTRPCContext({
-		session
+	return await createInnerTRPCContext({
+		headers: opts.req.headers
 	})
 }
 
 /**
  * 2. INITIALIZATION
  *
- * This is where the trpc api is initialized, connecting the context and
- * transformer
+ * This is where the tRPC API is initialized, connecting the context and transformer. We also parse
+ * ZodErrors so that you get typesafety on the frontend if your procedure fails due to validation
+ * errors on the backend.
  */
+
 const t = initTRPC.context<typeof createTRPCContext>().create({
-	transformer,
+	transformer: superjson,
 	errorFormatter({ shape, error }) {
 		return {
 			...shape,
@@ -81,31 +85,29 @@ const t = initTRPC.context<typeof createTRPCContext>().create({
 /**
  * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
  *
- * These are the pieces you use to build your tRPC API. You should import these
- * a lot in the /src/server/api/routers folder
+ * These are the pieces you use to build your tRPC API. You should import these a lot in the
+ * "/src/server/api/routers" directory.
  */
 
 /**
- * This is how you create new routers and subrouters in your tRPC API
+ * This is how you create new routers and sub-routers in your tRPC API.
+ *
  * @see https://trpc.io/docs/router
  */
 export const createTRPCRouter = t.router
 
 /**
- * Public (unauthed) procedure
+ * Public (unauthenticated) procedure
  *
- * This is the base piece you use to build new queries and mutations on your
- * tRPC API. It does not guarantee that a user querying is authorized, but you
- * can still access user session data if they are logged in
+ * This is the base piece you use to build new queries and mutations on your tRPC API. It does not
+ * guarantee that a user querying is authorized, but you can still access user session data if they
+ * are logged in.
  */
 export const publicProcedure = t.procedure
 
-/**
- * Reusable middleware that enforces users are logged in before running the
- * procedure
- */
+/** Reusable middleware that enforces users are logged in before running the procedure. */
 const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
-	if (!ctx.session?.user) {
+	if (!ctx.session || !ctx.session.user) {
 		throw new TRPCError({ code: 'UNAUTHORIZED' })
 	}
 	return next({
@@ -117,11 +119,10 @@ const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
 })
 
 /**
- * Protected (authed) procedure
+ * Protected (authenticated) procedure
  *
- * If you want a query or mutation to ONLY be accessible to logged in users, use
- * this. It verifies the session is valid and guarantees ctx.session.user is not
- * null
+ * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
+ * the session is valid and guarantees `ctx.session.user` is not null.
  *
  * @see https://trpc.io/docs/procedures
  */
