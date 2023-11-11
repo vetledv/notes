3fd1d3d58e17a4292b782570e535f0fedc67077b
Vetle de Vries <vetle1608@gmail.com>
2023-11-11 21:16:35 +0100

    revert back to nextauth v5 beta, fix trpc

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,4 +1,4 @@
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import NextAuth from 'next-auth'
 import Discord from 'next-auth/providers/discord'
 
